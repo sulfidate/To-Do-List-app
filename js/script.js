b695b304fd7793837d5059000589240cb7014a55
sulfidate <ffluid@me.com>
2021-05-24 23:39:23 +0200

    Update script.js
    write script by converting the JavaScript code into jQuery

---
+++ js/script.js
@@ -0,0 +1,34 @@
+function newItem() {
+	// add new item to list 
+	let li = $('<li></li>');
+	let inputValue = $('#input').val();
+	li.append(inputValue);
+	
+	if (inputValue === '') {
+		alert("You have to write something!");
+	} else {
+		$('#list').append(li);
+	}
+	
+	// crossing item out
+	function crossOut() {
+		li.toggleClass("strike")
+	}
+	
+	li.on("dblclick", function crossOut() {
+		li.toggleClass("strike");
+	});
+	
+	// add delete button
+	let crossOutButton = $('<crossOutButton></crossOutButton>');
+	crossOutButton.append(document.createTextNode('X'));
+	li.append(crossOutButton);
+	
+	crossOutButton.on("click", deleteListItem);
+	function deleteListItem() {
+		li.addClass("delete") 
+	}
+	
+	$('#list').sortable();
+	
+}
